0e71788d7a8371df73d94f2e95bb061598ca92f7
JustFlare <just1flare@gmail.com>
2017-09-06 20:42:30 +0300

    minor refactor

--- util.py
+++ util.py
@@ -3,11 +3,11 @@ import conf
 import os
 
 
-def current_date():
+def cur_date():
     return datetime.datetime.now().strftime('%m%d%H%M')
 
 
-def get_header(filename):
+def get_title(filename):
     with open(filename, 'r', encoding=conf.data_encoding) as f:
         return f.readline().replace('<h1>', '').replace('</h1>', '').replace('&quot;', '"')
 
